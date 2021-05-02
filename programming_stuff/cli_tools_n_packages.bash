36b410a8a9ddffd1441ea6aaef3385a65918a29f
arthur-yatsun <a_yatsun-itssv@kw.com>
2021-05-02 17:30:24 +0300

    refactor

--- programming_stuff/cli_tools_n_packages.bash
+++ programming_stuff/cli_tools_n_packages.bash
@@ -19,25 +19,40 @@ echo -e "\ninstall cli tools and packages"
 
 sudo apt install curl wget git vim xclip htop tmux zsh software-properties-common -y
 
+
 # install oh-my-zsh
-[ -d "$HOME/.oh-my-zsh/" ] && echo -e "\nohmyzsh is already installed" || \
-  (
-    echo -e "\ninstall ohmyzsh"
-    echo "y" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
+[ -d "$HOME/.oh-my-zdsh/" ] && echo -e "\nohmyzsh is already installed" || \
+(
+  echo -e "\ninstall ohmyzsh" && /
+  echo "y" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
+  echo "ohmyzsh was installed"
+)
+# set zsh as a defaul shell. password required
+# without sudo should work. If you use sudo it will change the shell not for your working user but for root
+# chsh -s $(which zsh)
 
-    # set zsh as a defaul shell. password required
-    # without sudo should work. If you use sudo it will change the shell not for your working user but for root
-    # chsh -s $(which zsh)
 
-    # install zsh plugins. to use add them into the plugins variable in your .zshrc file
-    # make sure that you define your plugins variable before you initialize zsh. https://stackoverflow.com/a/35929813
-    # zsh-autosuggestions. https://github.com/zsh-users/zsh-autosuggestions
-    git clone "https://github.com/zsh-users/zsh-autosuggestions" ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && /
-    # zsh-syntax-highlighting. https://github.com/zsh-users/zsh-syntax-highlighting
-    git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && /
+# install zsh plugins if INSTALL_OPTIONAL var is set
+# make sure that you define your plugins variable before you initialize zsh. https://stackoverflow.com/a/35929813
+if [ -d "$HOME/.oh-my-zsh/" ] && [ ! -v $INSTALL_OPTIONAL ]; then
+  : '
+  - zsh-autosuggestions          https://github.com/zsh-users/zsh-autosuggestions
+  - zsh-syntax-highlighting      https://github.com/zsh-users/zsh-syntax-highlighting
+  - zsh-autoswitch-virtualenv    https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv
+  - powerlevel10k theme          https://github.com/romkatv/powerlevel10k
+  '
+  echo -e "\ninstall zsh plugins"
 
-    # OPTIONAL. zsh-autoswitch-virtualenv. https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv. required python virtualenv installed. see `programming_stuff.bash` file
-    # git clone "https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git" ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autoswitch_virtualenv
-    # OPTIONAL. powerlevel10k theme. https://github.com/romkatv/powerlevel10k
-    # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-  )
+  [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ] && echo "zsh-autosuggestions is already installed" || \
+  git clone "https://github.com/zsh-users/zsh-autosuggestions" ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+  [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ] && echo "zsh-syntax-highlighting is already installed" || \
+  git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+  # virtualenv required
+  [ -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ] && echo "autoswitch_virtualenv is already installed" || \
+  git clone "https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git" ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autoswitch_virtualenv
+
+  [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autoswitch_virtualenv ] && echo "powerlevel10k theme is already installed" || \
+  git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git" ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
---
+++ user_configs.bash
@@ -0,0 +1,7 @@
+# disable ubuntu dock (it's up to u, I really don't like it) if INSTALL_OPTIONAL var is set
+if [ ! -v $INSTALL_OPTIONAL ]; then
+  gnome-extensions disable ubuntu-dock@ubuntu.com
+fi
+# to enable dock run `gnome-extensions enable ubuntu-dock@ubuntu.com`
+
+
