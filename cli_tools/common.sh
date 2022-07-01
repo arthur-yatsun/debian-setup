4aa6d7d02f47c106e6bcb4113384c52fb74e6f24
arthur-yatsun <arthuryatsun@arthuryatsun.com>
2022-07-01 21:09:27 +0300

    fix nvim setup and add gcc compiler

--- cli_tools/common.sh
+++ cli_tools/common.sh
@@ -8,4 +8,4 @@ echo -e "\ninstall cli tools and packages"
 : '
 '
 apt-get install curl vim wget git xclip htop zsh \
-    software-properties-common stow libreadline-dev ripgrep -y
+    software-properties-common stow libreadline-dev ripgrep build-essential -y
