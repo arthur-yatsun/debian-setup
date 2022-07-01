991c4fca16480a9210e2f59ad5640160bce45130
arthur-yatsun <arthuryatsun@arthuryatsun.com>
2022-07-01 20:59:29 +0300

    fix nvim installation

--- cli_tools/nvim.sh
+++ cli_tools/nvim.sh
@@ -9,7 +9,7 @@ echo -e "\ninstall neovim"
 check nvim ||
 (
   wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb -P /tmp && /
-  apt install ./nvim-linux64.deb
+  apt install /tmp/nvim-linux64.deb
   git clone --depth 1 https://github.com/wbthomason/packer.nvim\
     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 )
