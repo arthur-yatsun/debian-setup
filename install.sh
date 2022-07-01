caaf36871e07701763c380e8665b82081c9b266d
arthur-yatsun <arthuryatsun@arthuryatsun.com>
2022-07-01 13:54:18 +0300

    update installation script

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ set -e
 
 echo -e "\nstart the installation"
 
-apt-get update && apt-get upgrade -y
+# apt-get update && apt-get upgrade -y
 
 cd cli_tools/ && \
-    bash /utilities.sh
+    bash utilities.sh
 # bash ./programming_stuff/cli_tools_n_packages.bash && \
 # bash ./programming_stuff/languages.bash && \
 # bash ./programming_stuff/apps.bash && \
