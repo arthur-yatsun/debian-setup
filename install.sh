82ab966b0b8ff097f8cb777a876aeb239c33ecbd
arthur-yatsun <arthuryatsun@arthuryatsun.com>
2022-07-01 14:47:55 +0300

    update

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ cd cli_tools/
 
 bash common.sh bash && \
 bash zsh.sh && \
-bash tmux.sh && \
-bash programming_languages.sh && \
-bash databases.sh && \
-bash cloud_sdk.sh
+bash tmux.sh 
+# bash programming_languages.sh && \
+# bash databases.sh && \
+# bash cloud_sdk.sh
 
